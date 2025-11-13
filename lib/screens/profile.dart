1f61e03d82b30eb2b92b21a25089d210ad84d63e
Laith <laithalwani@gmail.com>
2025-11-13 18:09:09 -0500

    adds plus minus buttons and save button

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -4,13 +4,27 @@ import 'package:player_rating/models/app_user.dart';
 import 'package:player_rating/provider/auth_provider.dart';
 import 'package:player_rating/services/auth_service.dart';
 
-class Profile extends ConsumerWidget {
+class Profile extends ConsumerStatefulWidget {
   const Profile({super.key, required this.user});
 
   final AppUser user;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends ConsumerState<Profile> {
+  late int rating;
+  bool isUpdating = false;
+
+  @override
+  void initState() {
+    rating = widget.user.rating ?? 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final authState = ref.watch(authProvider);
 
     return authState.when(
@@ -19,7 +33,7 @@ class Profile extends ConsumerWidget {
         final canEdit = role == "admin";
         return Scaffold(
           appBar: AppBar(
-            title: Text("${user.displayName}'s Profile"),
+            title: Text("${widget.user.displayName}'s Profile"),
             actions: [
               IconButton(
                 onPressed: () async {
@@ -29,26 +43,77 @@ class Profile extends ConsumerWidget {
               ),
             ],
           ),
-          body: Center(
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 CircleAvatar(
-                  backgroundImage: NetworkImage(user.photoUrl ?? ""),
+                  backgroundImage: NetworkImage(widget.user.photoUrl ?? ""),
                   radius: 100,
                 ),
-                Text(user.displayName, style: const TextStyle(fontSize: 48)),
-                Text(user.email, style: const TextStyle(fontSize: 24)),
+                SizedBox(height: 32),
                 Text(
-                  (user.rating ?? 0).toString(),
-                  style: const TextStyle(fontSize: 32),
+                  widget.user.displayName,
+                  style: const TextStyle(fontSize: 48),
+                  textAlign: TextAlign.center,
                 ),
-                if (canEdit)
-                  ElevatedButton(
-                    onPressed: () {
-                      // add points to current player
-                    },
-                    child: const Icon(Icons.add),
-                  ),
+                Text(
+                  widget.user.email,
+                  style: const TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        (rating != widget.user.rating)
+                            ? setState(() {
+                                rating -= 1;
+                              })
+                            : null;
+                        // add points to current player
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color?>((
+                              Set<MaterialState> states,
+                            ) {
+                              if (rating == widget.user.rating) {
+                                return Colors.grey;
+                              }
+                              return null; // Use the component's default.
+                            }),
+                      ),
+                      child: const Icon(Icons.remove),
+                    ),
+                    Text(
+                      (rating).toString(),
+                      style: const TextStyle(fontSize: 32),
+                    ),
+                    if (canEdit)
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            rating += 1;
+                          });
+                          // add points to current player
+                        },
+                        child: const Icon(Icons.add),
+                      ),
+                  ],
+                ),
+                SizedBox(height: 32),
+                FilledButton(
+                  onPressed: () {
+                    print("Saving rating: $rating");
+
+                    // add points to current player
+                  },
+                  child: const Text("Save"),
+                ),
               ],
             ),
           ),
