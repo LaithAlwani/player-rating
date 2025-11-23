97436eb4fd7928866efb6b5aa6aa5faa0eb2b3bb
Laith <laithalwani@gmail.com>
2025-11-23 11:57:06 -0500

    adds position and lastlogin fields

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -6,17 +6,21 @@ class AppUser {
     required this.displayName,
     required this.displayNameLower,
     required this.email,
+    required this.position,
     this.rating = 0,
     this.photoUrl,
     this.isVerified = false,
     this.role = "user",
     Timestamp? createdAt,
     Timestamp? updatedAt,
+    Timestamp? lastLogin,
   }) : createdAt = createdAt ?? Timestamp.now(),
-       updatedAt = updatedAt ?? Timestamp.now();
+       updatedAt = updatedAt ?? Timestamp.now(),
+       lastLogin = lastLogin ?? Timestamp.now();
   final String uid;
   final String displayName;
   final String displayNameLower;
+  final String position;
   final String email;
   final String? photoUrl;
   final int? rating;
@@ -24,6 +28,7 @@ class AppUser {
   final String role;
   final Timestamp createdAt;
   final Timestamp updatedAt;
+  final Timestamp lastLogin;
 
   // ---------- Serialization ----------
 
@@ -37,6 +42,7 @@ class AppUser {
       uid: snapshot.id,
       displayName: data['displayName'],
       displayNameLower: data['displayNameLower'],
+      position: data['position'],
       email: data['email'],
       photoUrl: data['photoUrl'],
       rating: data['rating'],
@@ -44,6 +50,7 @@ class AppUser {
       role: data['role'] ?? 'user',
       createdAt: data['createdAt'] ?? Timestamp.now(),
       updatedAt: data['updatedAt'] ?? Timestamp.now(),
+      lastLogin: data['lastLogin'] ?? Timestamp.now(),
     );
   }
 
@@ -52,6 +59,7 @@ class AppUser {
       'uid': uid,
       'displayName': displayName,
       'displayNameLower': displayNameLower,
+      'position': position,
       'email': email,
       'photoUrl': photoUrl,
       'rating': rating,
@@ -59,6 +67,7 @@ class AppUser {
       'role': role,
       'createdAt': createdAt,
       'updatedAt': updatedAt,
+      'lastLogin': lastLogin,
     };
   }
 
@@ -66,24 +75,28 @@ class AppUser {
     String? uid,
     String? displayName,
     String? displayNameLower,
+    String? position,
     String? email,
     String? photoUrl,
     int? rating,
     bool? isVerified,
     String? role,
     Timestamp? updatedAt,
+    Timestamp? lastLogin,
   }) {
     return AppUser(
       uid: uid ?? this.uid,
       displayName: displayName ?? this.displayName,
       displayNameLower: displayNameLower ?? this.displayNameLower,
       email: email ?? this.email,
+      position: position ?? this.position,
       photoUrl: photoUrl ?? this.photoUrl,
       rating: rating ?? this.rating,
       isVerified: isVerified ?? this.isVerified,
       role: role ?? this.role,
       createdAt: createdAt,
       updatedAt: updatedAt ?? Timestamp.now(),
+      lastLogin: lastLogin ?? Timestamp.now(),
     );
   }
 }
